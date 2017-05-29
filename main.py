d518210baa2633f6a0e5f7a8b3034fb35c0a56d2
BuildTools <theguruofreason@gmail.com>
2017-05-29 14:05:28 -0700

    I don't recall

---
+++ main.py
@@ -0,0 +1,41 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+objpoints = []
+imgpoints = []
+
+def calibrate(fname):
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # find corners
+    ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+
+    # if found, add to our lists
+    if ret == True:
+        objpoints.append(objp)
+        imgpoints.append(corners)
+
+# grab images to calibrate
+calibration_images = glob.glob('./camera_cal/calibration*.jpg')
+
+# get corners in calibration images
+for fname in calibration_images:
+    calibrate(fname)
+
+def undistort(img, objpoints, imgpoints):
+    # Use cv2.calibrateCamera() and cv2.undistort()
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
+    img = cv2.drawChessboardCorners(img, (8,6), corners, ret)
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    undist = cv2.undistort(img, mtx, dist, None, mtx)
+    return undist
+
+import sobelize as sobel
+
